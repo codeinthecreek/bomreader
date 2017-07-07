e14c15111fab1f59404eafada861b3f5fa4ce921
codeinthecreek <37492900+codeinthecreek@users.noreply.github.com>
2017-07-07 16:10:08 +1000

    bom: crontab and script to download a set of weather observations

---
+++ getallobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+getweatherobs.sh brisbane
+getweatherobs.sh rockhampton
+getweatherobs.sh mackay
+getweatherobs.sh bowen
+getweatherobs.sh townsville
+getweatherobs.sh innisfail
+getweatherobs.sh Cairns_RC
+getweatherobs.sh Cairns
+getweatherobs.sh mareeba
+getweatherobs.sh cooktown
+
