39a086e7527c5134834ac75edd8891ca0a8f5726
codeinthecreek <37492900+codeinthecreek@users.noreply.github.com>
2018-03-19 18:10:05 +1000

    getallobs.sh: added missing supported location, Proserpine, to locations downloaded

--- getallobs.sh
+++ getallobs.sh
@@ -2,6 +2,7 @@
 getweatherobs.sh brisbane
 getweatherobs.sh rockhampton
 getweatherobs.sh mackay
+getweatherobs.sh proserpine
 getweatherobs.sh bowen
 getweatherobs.sh Townsville_AP
 getweatherobs.sh Townsville_MS
